b6d728c60de1812832e073663a17f413806ae2c3
Sahlre <shlondon8@gmail.com>
2017-07-02 17:56:49 -0500

    Se modificó Informe.pdf

--- Script/Analisis/Tree.R
+++ Script/Analisis/Tree.R
@@ -1,6 +1,6 @@
 
-setwd("D:/Santiago/2017/Cua/Datathon/Bases de datos")
-library (tree)
+
+#Bases de datos
 bd_Train1 <- read.csv("bd_Train.csv", colClasses = "factor")
 
 bd_Valid1 <- read.csv("bd_Test.csv", colClasses = "factor")
@@ -21,24 +21,20 @@ bd_Valid1$P5010 <- as.integer(bd_Valid1$P5010)
 bd_Train1$P6008 <- as.integer(bd_Train1$P6008)
 bd_Valid1$P6008 <- as.integer(bd_Valid1$P6008)
 
-treemodel <- tree(P6210nuevo ~ .,
-                  data = bd_Train1)
-save(treemodel, file = "treemodel.rda")
+#Libreria que permite correr una árbol de clasificación
+library(tree)
+
+#Se ajusta el modelo
+treemodel <- tree(P6210nuevo ~ ., data = bd_Train1)
+
+#Precisión en datos de entrenamiento
+tree.pred <- predict(treemodel, bd_Train1[,-61], type = "class")
+mean(tree.pred==bd_Train1$P6210nuevo)
+
+#Precisión en datos de prueba
 tree.pred <- predict(treemodel, bd_Valid1[,-61], type = "class")
 mean(tree.pred==bd_Valid1$P6210nuevo)
 
-#Podando variables no importantes
-system.time(cv.treemodel <- cv.tree(treemodel, FUN = prune.misclass))
-
-
-
-tree_podado <- prune.misclass(treemodel, best = 8)
-plot(tree_podado)
-text(tree_podado,pretty = 0)
-tree.pred.podado <- predict(tree_podado,bd_Valid1[,-61],type = "class")
-mean(tree.pred==bd_Valid1$P6210nuevo)
-
-save(cv.treemodel, file = "cv_treemodel.rda")
-
-
-
+#Se guarda el modelo estimado, esto es necesario para luego
+#gráficar el árbol de clasificación.
+save(treemodel, file = "treemodel.rda")
