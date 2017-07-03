43cb4dd9a30fb0ea9198614333837f1ab9d9d122
Sahlre <shlondon8@gmail.com>
2017-07-02 20:06:26 -0500

    Se modificó Informe.pdf

--- Script/Analisis/Boost.R
+++ Script/Analisis/Boost.R
@@ -1,8 +1,8 @@
+
+#Librería para estimar Boosting
+library(gbm)
 #Bases de datos
-setwd("D:/Santiago/2017/Cua/Datathon/Bases de datos")
-
 bd_Train1 <- read.csv("bd_Train.csv", colClasses = "factor")
-
 bd_Valid1 <- read.csv("bd_Test.csv", colClasses = "factor")
 
 #Se elimnan las dos primeras columnas
@@ -22,16 +22,16 @@ bd_Valid1$P5010 <- as.integer(bd_Valid1$P5010)
 bd_Train1$P6008 <- as.integer(bd_Train1$P6008)
 bd_Valid1$P6008 <- as.integer(bd_Valid1$P6008)
 
-library(gbm)
+#Se estima el algoritmo Boosting en Árboles de Decisión.
 modeloBoos <- gbm(P6210nuevo ~ .,
                   distribution = "multinomial",
-                  n.trees = 500,
+                  n.trees = 100,
                   interaction.depth = 4,
                   data = bd_Train1)
-summary(modeloBoos)
 
-predBoos <- predict(modeloBoos, 
-                    newdata = bd_Valid1[,-61], 
+#Precisión en datos de prueba
+predBoos <- predict(modeloBoos,
+                    newdata = bd_Valid1[,-61],
                     n.trees = 100,
                     type = "response")
 
@@ -40,3 +40,15 @@ predBoosCateg <- ifelse(predBoos == 1, "Alto",
                         ifelse(predBoos == 2, "Bajo", "Medio"))
 
 mean(predBoosCateg == bd_Valid1$P6210nuevo)
+
+#Precisión en datos de entrenamiento
+predBoos <- predict(modeloBoos,
+                    newdata = bd_Train1[,-61],
+                    n.trees = 100,
+                    type = "response")
+
+predBoos <- apply(predBoos,1,which.max)
+predBoosCateg <- ifelse(predBoos == 1, "Alto",
+                        ifelse(predBoos == 2, "Bajo", "Medio"))
+
+mean(predBoosCateg == bd_Train1$P6210nuevo)
